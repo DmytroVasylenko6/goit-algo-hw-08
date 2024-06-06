fd07c691145f3a3163ba1e7d82b6f384550fc183
DmytroVasylenko6 <tredstoun651@ukr.net>
2024-06-07 00:41:32 +0300

    fix visualize_connections

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def connect_cables(cables):
         cable1 = heapq.heappop(heap)
         cable2 = heapq.heappop(heap)
 
-        merged_cable = (cable1[0] + cable2[0], f"{cable1[1]}{cable2[1]}")
-        visualize_connections += f"({cable1[1]} > {cable2[1]}) { "=> " if len(heap) >= 1 else ""}" 
+        merged_cable = (cable1[0] + cable2[0], f"{cable1[1]}+{cable2[1]}")
+        visualize_connections += (
+            f"({cable1[1]} > {cable2[1]}) {' => ' if len(heap) > 1 else ''}"
+        )
 
         heapq.heappush(heap, merged_cable)
         expenses += merged_cable[0]
